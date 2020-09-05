d8fe8bafd4b98b9a7ef8641a08acb1bc1d42776d
JoyOliveira <joyce.lvr@gmail.com>
2020-09-05 00:44:39 -0300

    imprime ate classes[]

--- index.js
+++ index.js
@@ -1,26 +1,15 @@
-const csv = require('csv-parser');
 const csvjson = require('csvtojson');
+const csv = require('csv-parser');
 const fs = require('fs');
 const _ = require('lodash');
 const { group } = require('console');
 const { reduce, uniqueId, first, result, iteratee } = require('lodash');
 
-
-const results = [];
- 
-fs.createReadStream('input.csv')
-  .pipe(csv())
-  .on('data', (data) => results.push(data))
-  .on('end', () => {
-    //console.log(results);
-
-  });
-
-
-
 csvjson().fromFile('input.csv')
     .then(entrada => {
-        
+      
+      //console.log(entrada[2]);
+      
         let arrayOutPut = [...entrada.reduce((hash, { fullname, eid, classes}) => {
             let arrayTemporaria = hash.get(eid) || { fullname, eid, classes: []};
             
@@ -30,13 +19,13 @@ csvjson().fromFile('input.csv')
             return hash.set(eid, arrayTemporaria);
           }, new Map).values()];
         
-          //console.log(arrayOutPut);
+          console.log(arrayOutPut);
 
           fs.writeFile('output.json', arrayOutPut, (err) => {
                       if (err) {
                           throw err;
                       }
-                      console.log("JSON salvo!")
+             //         console.log("JSON salvo!")
                 })
           
           }).catch(err => {
